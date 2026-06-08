55c439154787e6db97f840314bde6706df15f382
Deva Shukla <shukladeva82@gmail.com>
2026-06-09 00:20:07 +0530

    commit config done

--- Day1/1.4.py
+++
@@ -1,4 +0,0 @@
-#Variables -> A variable is a container for storing data values. In Python, you can create a variable and assign it a value using the assignment operator (=) and can be used for future reference. 
-
-Name = input("What is your name? ")
-
--- Day1/String.py
+++ Day1/String.py
@@ -8,3 +8,8 @@ Last_name = "Shukla"
 Full_name = First_name + " " + Last_name
 print(Full_name)
 print("Deva" + " " + "Loves" + " " + "Python")
+
+print(len(name)) # len() => Used to get the length of a string. It returns the number of characters in the string, including spaces and punctuation.
+
+print(len(input("Enter your name: "))) # I/P => "Deva Shukla"
+# O/P => 11 !0 Char + 1 Space = 11 Characters
