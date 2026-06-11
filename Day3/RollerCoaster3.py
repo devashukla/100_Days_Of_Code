8521a83e14367ccbf865c2febd654dfc5f7e431a
Deva Shukla <shukladeva82@gmail.com>
2026-06-11 12:05:15 +0530

    commit change done

--- Day3/RollerCoaster3.py
+++ Day3/RollerCoaster3.py
@@ -4,9 +4,9 @@ Height = int(input("Please Enter your height in cm."))
 if Height > 120:
     print("You can ride")
     Age = int(input("Please Enter your Age"))
-    if Age > 18:
+    if Age >= 18:
         print("Pay $12")
-    elif Age > 12 & Age < 18:
+    elif Age >= 12 :
         print("Pay $7")
     else:
         print("Pay $5")
