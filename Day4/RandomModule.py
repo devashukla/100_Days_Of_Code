300853b432e9886e745ac6f5d04b4e8b629ef73c
Deva Shukla <shukladeva82@gmail.com>
2026-06-12 23:25:02 +0530

    Day 4: Randomisation and Python Lists

---
+++ Day4/RandomModule.py
@@ -0,0 +1,8 @@
+import random 
+
+ #random.randint(start, stop)
+ #random.randint => used to return random Integer value.
+print(random.randint(1,21))
+
+# random.random => used to return random float value from 0 to 1.
+print(random.random())
